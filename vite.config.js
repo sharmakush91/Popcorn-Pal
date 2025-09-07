69d02eda23a134bbf6071242d0a06e2822ce43fe
kush <128039250+sharmakush91@users.noreply.github.com>
2025-09-07 15:57:49 +1200

    changed the base to / on vite.config for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // Important for Netlify relative paths
+  base: "/", // Important for Netlify relative paths
   plugins: [react()],
 });
